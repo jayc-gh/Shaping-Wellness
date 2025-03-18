528690651ef07f3bae66b82a394f3c7519bdf639
Jay Cho <jaycho1017@gmail.com>
2025-03-17 23:26:24 -0500

    created donate branch. started integrating stripe

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import CheckoutPage from '@/components/CheckoutPage';
+import DonationPage from '@/components/donationPage';
 import convertToSubcurrency from '@/lib/convertToSubcurrency';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
@@ -11,13 +11,31 @@ if (!stripePublicKey) {
   throw new Error('Missing NEXT_PUBLIC_STRIPE_PUBLIC_KEY');
 }
 
-const stripeSecretKey: string = process.env.STRIPE_SECRET_KEY as string;
-if (!stripeSecretKey) {
-  throw new Error('Missing STRIPE_SECRET_KEY');
-}
-
 const stripePromise = loadStripe(stripePublicKey);
 
 export default function Donate() {
-  return <div className="py-30">donation page</div>;
+  const amount = 49.99;
+
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ${amount}</span>
+        </h2>
+      </div>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: 'payment',
+          amount: convertToSubcurrency(amount),
+          currency: 'usd',
+        }}
+      >
+        <DonationPage amount={amount} />
+      </Elements>
+    </main>
+  );
 }
