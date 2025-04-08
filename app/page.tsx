03c7a35018752bb3f0e37bd56c708d109fb6b114
Jay Cho <jaycho1017@gmail.com>
2025-04-07 22:10:55 -0500

    added homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,58 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="flex flex-col">
+      {/* big section with 1 column */}
+      <section className="flex big-section-h items-center bg-amber-200">
+        <div className="flex flex-col items-start pl-60 w-222">
+          <h1 className="space-y-5 mb-8">
+            <span>Healthy Futures,</span>
+            <br />
+            <span>
+              <span className="text-orange-400">Stronger</span> Communities
+            </span>
+          </h1>
+          <h4 className="mb-5">
+            Join us in creating opportunities for girls to grow stronger, more
+            confident, and healthier. Partner today and be part of the change.
+          </h4>
+          <button className="border rounded-xl py-3 px-5">
+            <h4 className="font-bold">Become a partner</h4>
+          </button>
+        </div>
+      </section>
+
+      {/* small section with just text */}
+      <section className="flex items-center justify-center sm-section-h">
+        <div className="flex flex-col items-center justify-center w-180">
+          <h4 className="mb-8">OUR MISSION</h4>
+          <h4 className="text-center text-black">
+            At Shaping Wellness, our mission is to cultivate a generation of
+            resilient and confident young women equipped with the tools and
+            knowledge to prioritize their health and well-being, ultimately
+            building stronger, healthier communities.
+          </h4>
+        </div>
+      </section>
+
+      {/* big section no content */}
+      <section className="flex big-section-h items-center bg-amber-200"></section>
+
+      {/* small section with just text */}
+      <section className="flex items-center justify-center sm-section-h">
+        <div className="flex flex-col items-center justify-center w-180">
+          <h4 className="mb-8">PROGRAMS</h4>
+          <h4 className="text-center text-black mb-8">
+            We offer dynamic programs that promote physical activity, mental
+            wellness, and self-care, equipping girls with the knowledge and
+            support they need to lead healthy lives.
+          </h4>
+          <button className="border rounded-xl px-5 py-3">
+            <h4 className="font-bold">Learn more</h4>
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,7 +32,7 @@ export default function NavBar() {
   const linkStyles = 'block px-4 py-2 hover:bg-gray-100 hover:rounded-md w-35';
 
   return (
-    <nav className="flex justify-between items-center w-full bg-white shadow-sm z-999 text-black h-20">
+    <nav className="flex justify-between items-center w-full bg-white shadow-sm z-999 text-black navbar-h">
       {/* left/logo side of navbar */}
       <div className="max-w-7xl px-4 py-3">
         <Link
