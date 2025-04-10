b822e328eb1df8ae3e1f9e862f974ff7ee38f6db
Jay Cho <jaycho1017@gmail.com>
2025-04-10 17:54:53 -0500

    fixing git empty files errors

---
+++ app/donate/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import DonateForm from '@/components/donationForm';
+export default function DonatePage() {
+  return <DonateForm />;
+}
+
+// make new component for successful donation (step 4)
+// sending form elements/variables into excel
+
+// autofill address later
---
+++ app/get-involved/volunteer/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Volunteer() {
+  return (
+    <div className="py-30">
+      <Link href="/get-involved/volunteer/volunteer-form">Volunteer Form</Link>
+    </div>
+  );
+}
