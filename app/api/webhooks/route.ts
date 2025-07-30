e877c07b2baf4e2b08e55d434b755aeb7c7e86f5
Jay Cho <jaycho1017@gmail.com>
2025-07-30 00:56:58 -0500

    email info

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -65,8 +65,8 @@ async function processEvent(event: Stripe.Event) {
       }
       console.log('Payment intent succeeded');
       const emailInfo = await getEmailInfoFromPaymentIntent(intent);
+      console.log('emailInfo', emailInfo);
       if (emailInfo) {
-        console.log('emailInfo', emailInfo);
         const emailSent = await sendSuccessEmail(emailInfo);
         const receiptSent =
           emailSent.status == 201 || emailSent.status == 200 ? true : false;
